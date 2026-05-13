17fcfd7cbe05cbd0a1e23613b3a6505270a010a4
Nabin shahi <nabinshahi657@gmail.com>
2026-05-13 23:02:25 +0545

    initial commit

---
+++ data.py
@@ -0,0 +1,23 @@
+items={1,2,3,'ram','shyam',4,5}
+items2={8,9,10,'ram','shyam'}
+items3=items.difference(items2)
+items4=items.symmetric_difference(items2)
+items5=items.intersection(items2)
+items6=items.union(items2)
+items7=items.isdisjoint(items2)
+items8=items.issubset(items2)
+items9=items.issuperset(items2)
+print(items3)
+print(items4)
+print(items5)
+print(items6)
+print(items7)
+print(items8)
+print(items9)
+
+
+
+
+# dictionary
+student_marks={'ram':93,'shyam':98,'hari':88}
+result=student_marks.get('hira,'students not found')
